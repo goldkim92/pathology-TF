69894739a9bf9dbfa73df4af363474573b793d7e
goldkim92 <goldkim92@gmail.com>
2018-06-21 15:51:30 +0900

    train_files read when continue_train

--- model.py
+++ model.py
@@ -80,29 +80,37 @@ class Network(object):
     def train(self):
         
         # load train-data & valid-data file list (label & image file)
-        files = glob(os.path.join('data','224','*')) # len(files) = 6985
-        usable_files = [file for file in files if re.split('[/_.]+', file)[2] in self.labels_dic.keys()] # len(usable_files) = 5000
-        np.random.shuffle(usable_files)
-        train_files = usable_files[:4000] # 4000
-        valid_files = usable_files[4000:4500] # 500
-        test_files = usable_files[4500:] # 500
+        if self.continue_train:
+            train_files = list()
+            valid_files = list()
+            with open(os.path.join('assets','conv_drop','test', 'train_files.txt'), 'r') as f:
+                train_files = f.read().splitlines()
+            with open(os.path.join('assets','conv_drop','test', 'valid_files.txt'), 'r') as f:
+                valid_files = f.read().splitlines()
+            
+        else: # self.continue_train == False
+            files = glob(os.path.join('data','224','*')) # len(files) = 6985
+            usable_files = [file for file in files if re.split('[/_.]+', file)[2] in self.labels_dic.keys()] # len(usable_files) = 5000
+            np.random.shuffle(usable_files)
+            train_files = usable_files[:4000] # 4000
+            valid_files = usable_files[4000:4500] # 500
+            test_files = usable_files[4500:] # 500
+
+            # save test_files list in txt format
+            test_txt = os.path.join(self.test_dir, 'test_files.txt')
+            valid_txt = os.path.join(self.test_dir, 'valid_files.txt')
+            train_txt = os.path.join(self.test_dir, 'train_files.txt')
+            with open(test_txt, 'a') as f:
+                for file in test_files:
+                    f.write(file + '\n')
+            with open(valid_txt, 'a') as f:
+                for file in valid_files:
+                    f.write(file + '\n')
+            with open(train_txt, 'a') as f:
+                for file in train_files:
+                    f.write(file + '\n')
         
-        batch_idxs = len(train_files) // self.batch_size
-        
-        # save test_files list in txt format
-        test_txt = os.path.join(self.test_dir, 'test_files.txt')
-        valid_txt = os.path.join(self.test_dir, 'valid_files.txt')
-        train_txt = os.path.join(self.test_dir, 'train_files.txt')
-        with open(test_txt, 'a') as f:
-            for file in test_files:
-                f.write(file + '\n')
-        with open(valid_txt, 'a') as f:
-            for file in valid_files:
-                f.write(file + '\n')
-        with open(train_txt, 'a') as f:
-            for file in train_files:
-                f.write(file + '\n')
-                
+        batch_idxs = len(train_files) // self.batch_size        
         
         # variable initialize
         self.sess.run(tf.global_variables_initializer())
@@ -297,9 +305,11 @@ class Network(object):
         print(" [*] Reading checkpoint...")
         
         ckpt = tf.train.get_checkpoint_state(self.ckpt_dir)
+        # ckpt = tf.train.get_checkpoint_state(os.path.join('assets','conv_drop','checkpoint'))
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
             self.saver.restore(self.sess, os.path.join(self.ckpt_dir, ckpt_name))
+            # self.saver.restore(self.sess, os.path.join(os.path.join('assets','conv_drop','checkpoint'), ckpt_name))
             return True
         else:
             return False
